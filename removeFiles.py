397ef901b7a0c9acba2f73050eed558949e28cb8
RASESH-THECODER <62789714+RASESH-THECODER@users.noreply.github.com>
2020-10-08 21:51:16 +0530

    Add files via upload

---
+++ removeFiles.py
@@ -0,0 +1,10 @@
+import os, stat
+import shutil
+import time 
+
+def remove_readonly(func, path, _):
+    "Clear the readonly bit and reattempt the removal"
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+shutil.rmtree(directory, onerror=remove_readonly)
